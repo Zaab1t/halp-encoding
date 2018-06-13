48673f0581478709f77b530b3f4a7a8caab09889
Carl Bordum Hansen <carl@bordum.dk>
2018-06-13 16:58:42 +0200

    Add initial example.py

---
+++ example.py
@@ -0,0 +1,10 @@
+# coding=halp
+
+input('Press enter to run the line `help(if)`')
+help(if)
+
+input('Press enter to run the line `help(with)`')
+help(with)
+
+input('Press enter to run the line `help(except)`')
+help(except)
